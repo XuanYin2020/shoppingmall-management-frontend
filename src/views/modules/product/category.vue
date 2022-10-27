54fb58070e995893b5ed53ae2b7c955e008bca0a
XuanYin <xuanyin2019@gmail.com>
2022-10-27 17:20:58 +1100

    Initial commit

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <el-tree
+      :data="data"
+      :props="defaultProps"
+      @node-click="handleNodeClick"
+    ></el-tree>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      data: [],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
+    }
+  },
+  methods: {
+    handleNodeClick (data) {
+      console.log(data)
+    },
+    getMenus () {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/list/tree'), // 请求的地址
+        method: 'get'
+        //   params: this.$http.adornParams({
+        //     'page': this.pageIndex,
+        //     'limit': this.pageSize,
+        //     'roleName': this.dataForm.roleName
+        //   })
+      }).then(({data}) => { // 成功了怎么办
+        console.log('成功获取到菜单数据', data)
+      })
+    }
+  },
+  created () {
+    this.getMenus()
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
