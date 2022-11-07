6e51d89dbe8b57130969ab6900b6b0ae0d2937eb
XuanYin <xuanyin2019@gmail.com>
2022-11-07 16:55:58 +1100

    删除功能的页面效果

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="container">
-    <el-tree
-      :data="menus"
-      :props="defaultProps"
-      @node-click="handleNodeClick"
-    ></el-tree>
+    <el-tree :data="menus" show-checkbox node-key="catId" :props="defaultProps"  :expand-on-click-node="false">
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button v-if="node.level<=2" type="text" size="mini" @click="() => append(data)"> Append </el-button>
+          <el-button v-if="node.childNodes.length==0" type="text" size="mini" @click="() => remove(node, data)"> Delete </el-button>
+        </span>
+      </span>
+    </el-tree>
   </div>
 </template>
 
@@ -20,9 +24,6 @@ export default {
     }
   },
   methods: {
-    handleNodeClick (data) {
-      console.log(data)
-    },
     getMenus () {
       this.$http({
         url: this.$http.adornUrl('/product/category/list/tree'), // 请求的地址
@@ -36,6 +37,13 @@ export default {
         console.log('成功获取到菜单数据', data.data)
         this.menus = data.data
       })
+    },
+    // 发送请求实现
+    append (data) {
+      console.log('append', data)
+    },
+    remove (node, data) {
+      console.log('remove', node, data)
     }
   },
   created () {
